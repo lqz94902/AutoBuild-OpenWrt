0d345d716783bbea3ee425431de406498b1323b5
JonasL <460363851@qq.com>
2020-01-07 13:34:23 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,5 +5,7 @@
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
-#1. Modify default IP
+#1. 修改默认 IP
 sed -i 's/192.168.1.1/192.168.123.1/g' openwrt/package/base-files/files/bin/config_generate
+#2. 修改 hosts
+sed -i '/ff02::2 ip6-allrouters/a 192.168.123.1 k2p.me k2p.com' openwrt/package/base-files/files/etc/hosts
